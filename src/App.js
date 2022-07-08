588db9a1e320d58ba07d9e2a283aef53e9c190f9
faezeh-fazi <76036868+faezeh-fazi@users.noreply.github.com>
2022-07-08 15:51:24 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes, useParams } from "react-router-dom";
+import Intro from "./screen/Intro";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Intro />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
