9c9796a6c8ef1f89393acedcf04d546ca46be4d4
faezeh-fazi <76036868+faezeh-fazi@users.noreply.github.com>
2022-07-08 15:51:02 +0300

    7.8

---
+++ src/screen/Intro.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import background from '../assets/firstbackground.jpg'
+import "../style/main.css"
+const Intro = () => {
+  return (
+    <>
+  <div class="content">
+    <h2>Spyfall</h2>
+    <h2>Spyfall</h2>
+  </div>
+  
+    </>
+  )
+}
+
+export default Intro
